b86c59d32aba097171c8cce9e6f9e57a02bef1c4
felipevcc <felipevillamizarc@gmail.com>
2024-03-12 21:46:44 -0500

    Documentation of service interfaces

--- pokemon-api/src/main/java/com/accedotech/pokemonapi/service/PokemonService.java
+++ pokemon-api/src/main/java/com/accedotech/pokemonapi/service/PokemonService.java
@@ -3,9 +3,15 @@ package com.accedotech.pokemonapi.service;
 import com.accedotech.pokemonapi.dto.pokemon.PokemonDataDTO;
 import com.accedotech.pokemonapi.dto.pokemon.PokemonsPageDTO;
 
+/**
+ * Interface that represents the pokemon service.
+ * Contains methods with the necessary functionalities for pokémon queries.
+ */
 public interface PokemonService {
 
+    // Method to obtain all paged pokémon
     PokemonsPageDTO getAllPokemons(Integer page, Integer pageSize);
 
+    // Method to obtain a pokémon by its id
     PokemonDataDTO getPokemonById(Long pokemonId);
 }
--- pokemon-api/src/main/java/com/accedotech/pokemonapi/service/UserService.java
+++ pokemon-api/src/main/java/com/accedotech/pokemonapi/service/UserService.java
@@ -5,15 +5,24 @@ import com.accedotech.pokemonapi.dto.user.UserDTO;
 import com.accedotech.pokemonapi.dto.user.UserRegisterDTO;
 import com.accedotech.pokemonapi.dto.user.UserUpdateDTO;
 
+/**
+ * Interface that represents the user management service.
+ * Contains methods with the necessary functionalities for user management.
+ */
 public interface UserService {
 
+    // User login method
     UserDTO userLogin(LoginDTO loginData);
 
+    // Method to get a user by id
     UserDTO getUserById(Long userId);
 
+    // Method to register a new user in the system
     UserDTO registerUser(UserRegisterDTO userData);
 
+    // Method to update user information
     UserDTO updateUser(Long userId, UserUpdateDTO userData);
 
+    // Method to update the user's password
     UserDTO updatePassword(Long userId, String currentPassword, String newPassword);
 }
