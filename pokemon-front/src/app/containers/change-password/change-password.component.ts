1fdfa4443951e83660b5227ef991a09a924a5ba1
felipevcc <felipevillamizarc@gmail.com>
2024-03-16 19:15:14 -0500

    User data and password update views

--- pokemon-front/src/app/containers/change-password/change-password.component.ts
+++ pokemon-front/src/app/containers/change-password/change-password.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Paths } from '@enums/paths.enum';
+import { PasswordChangeReq } from '@models/user/pwd-change-req.interface';
+import { UserService } from '@services/user/user.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-change-password',
@@ -6,5 +12,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./change-password.component.scss']
 })
 export class ChangePasswordComponent {
+
+  form!: FormGroup;
+
+  constructor (
+    private userService: UserService,
+    private messageService: MessageService,
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    this.messageService.clear();
+
+    const formData = this.form.value;
+
+    if (formData.password !== formData.confirmPassword) {
+      this.messageService.add({severity: 'error', detail: 'Passwords do not match'});
+      return;
+    }
+
+    const passwordData: PasswordChangeReq = {
+      newPassword: formData.password
+    };
+
+    this.userService.changePassword(passwordData).subscribe({
+      next: () => {
+        this.router.navigate([Paths.Pokemons]);
+      },
+      error: () => {
+        this.messageService.add({severity: 'error', detail: 'Error updating password'});
+      }
+    });
+  }
+
+  onPokemonsClick(): void {
+    this.router.navigate([Paths.Pokemons]);
+  }
 
 }
--- pokemon-front/src/app/containers/update-user/update-user.component.ts
+++ pokemon-front/src/app/containers/update-user/update-user.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Messages } from '@enums/messages.enum';
+import { Paths } from '@enums/paths.enum';
+import { UserUpdateReq } from '@models/user/user-update-req.interface';
+import { User } from '@models/user/user.interface';
+import { AuthService } from '@services/auth/auth.service';
+import { UserService } from '@services/user/user.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-update-user',
@@ -6,5 +15,81 @@ import { Component } from '@angular/core';
   styleUrls: ['./update-user.component.scss']
 })
 export class UpdateUserComponent {
+
+  form!: FormGroup;
+  isLoading: boolean = true;
+  user!: User;
+  
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+    private messageService: MessageService,
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      email: ['', Validators.required]
+    });
+    this.isLoading = true;
+    this.getUserData();
+  }
+
+  getUserData(): void {
+    this.userService.getUserData().subscribe({
+      next: (data: User) => {
+        this.user = data;
+        this.form.patchValue({
+          firstName: this.user.firstName,
+          lastName: this.user.lastName,
+          phoneNumber: this.user.phoneNumber,
+          email: this.user.email
+        });
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.isLoading = false;
+        console.error(error);
+        this.messageService.add({severity: 'error', detail: Messages.ERROR_GET});
+      }
+    });
+  }
+
+  onSubmit() {
+    this.messageService.clear();
+
+    const formData = this.form.value;
+
+    const userData: UserUpdateReq = {
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      phoneNumber: formData.phoneNumber,
+      email: formData.email
+    };
+
+    this.userService.updateUser(userData).subscribe({
+      next: () => {
+        if (this.user.email !== formData.email) {
+          this.authService.logout();
+        }
+        this.router.navigate([Paths.Login]);
+      },
+      error: (error) => {
+        let msg = Messages.ERROR_UPDATE;
+        if (error.error) {
+          msg = error.error;
+        }
+        this.messageService.add({severity: 'error', detail: msg});
+      }
+    });
+  }
+
+  onPokemonsClick(): void {
+    this.router.navigate([Paths.Pokemons]);
+  }
 
 }
